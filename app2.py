744587a5ab99875967987530a9515ad267ca611c
Andy <148748006+madand1@users.noreply.github.com>
2024-05-08 14:06:07 +0200

    Update app2.py

--- app2.py
+++ app2.py
@@ -10,7 +10,7 @@ load_dotenv(dotenv_path="dotenv.env")
 
 # URL
 url_base="https://api.themoviedb.org/3/"
-# Variable key, guardamos por el diccionario os.environ nuestra key
+# Variable KEY
 key=os.environ["api_key"]
 # Código del país
 code='EN'
@@ -18,10 +18,10 @@ code='EN'
 payload = {'api_key':key,"languaje":'en-EN'}
 #Pedimos el título de una película por teclado y nos muestra el json de TheMovieDB
 pelicula = input("Introduce el nombre de la pelicula: ")
-# URL con los parámetros
+
 url = url_base+'search/movie'+'?query='+pelicula
 r=requests.get(url,params=payload)
-#Guardamos en una variable el contenido de la respuesta
+
 if r.status_code == 200:
     doc=r.json()
     #Mostramos el contenido de la respuesta
