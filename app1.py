654af368dfc3ee2acc531ccfb9f057bf42d08920
Andy <148748006+madand1@users.noreply.github.com>
2024-05-08 14:03:25 +0200

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,40 @@
+# Importamos las bibliotecas necesarias
+import requests
+import json
+import os
+from dotenv import load_dotenv
+
+# Cargamos las variables de entorno desde el archivo dotenv.env
+load_dotenv(dotenv_path="dotenv.env")
+
+# Guardamos la URL base
+url_base = "https://api.themoviedb.org/3/"
+
+# Obtenemos la clave de la API desde las variables de entorno
+api_key = os.getenv("API_KEY")
+
+# Si la clave de la API no está configurada, mostramos un mensaje de error
+if api_key is None:
+    print("Error: No se encontró la clave de la API en el archivo dotenv.env.")
+    exit()
+
+# Código del país, en este caso Inglaterra
+country_code = os.getenv("COUNTRY_CODE", "EN")
+
+# Pedir la ID de la película
+movie_id = input("Introduce la ID de la película: ")
+
+# URL con los parámetros
+url = f"{url_base}movie/{movie_id}"
+payload = {'api_key': api_key, 'countryCode': country_code}
+response = requests.get(url, params=payload)
+
+# Guardamos en una variable el contenido de la respuesta
+if response.status_code == 200:
+   
+    movie_data = response.json()
+    
+    print(json.dumps(movie_data, indent=4, sort_keys=True))
+else:
+    
+    print("Error:", response.status_code)
---
+++ app3.py
@@ -0,0 +1,36 @@
+# Bibliotecas necesarias
+import requests
+import json
+import os
+from dotenv import load_dotenv
+
+# Variables de entorno desde el archivo dotenv.env
+load_dotenv(dotenv_path="dotenv.env")
+
+# Guardamos la url base
+url_base="https://api.themoviedb.org/3/"
+
+# En una variable key, guardamos por el diccionario os.environ nuestra key
+key=os.environ["api_key"]
+
+# Código del país, en esta caso Inglaterra
+code='en-US'
+
+# Diccionario que guarde nuestros parámetros
+payload = {'api_key':key,"languaje":code}
+
+# Peticion de titulo 
+serie = input("Introduce el nombre de la serie o tv show que desea buscar: ")
+
+# Construimos la url con los parámetros
+url = url_base+'search/tv'+'?query='+serie
+r=requests.get(url,params=payload)
+
+# Guardo la variable de la respuesta
+if r.status_code == 200:
+    doc=r.json()
+    
+    print(json.dumps(doc, indent=4, sort_keys=True))
+else:
+    print ("Error")
+    print (r.status_code)
